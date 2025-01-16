b96e40c25b2943f90662209cd41f5a973e5d079f
keybody65 <popov27051971@gmail.com>
2025-01-16 13:27:29 +0300

    UPD_2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # This is my app for calculating linear algebra equations
+
+### UPD_1
+##### A trial version was created to identify any bugs.
+
+### UPD_2
+##### I rewrote the method of adding matrices (now you can upload not only one matrix), in addition, the function **show** and the function **my_matrix** were added to view all added matrices.
--- linear_algebric_base_algoritms.py
+++ linear_algebric_base_algoritms.py
@@ -1,36 +1,59 @@
 class Linear_Bsk_Operations:
-	def __init__(self, n:int, m:int):
-		self.n = n
-		self.m = m
-		self.matrix = [[0] * m for i in range(n)]
+	def __init__(self):
+		self.n = 0
+		self.m = 0
+		self.arr_matrix = []
+
+	def add_matrix(self, n:int, m:int):
+		new_matrix = [[0] * m for _ in range(n)]
+		print(f"Введите элементы матрицы размером {n}x{m}:")
 		for i in range(n):
-			for j in range(m):
-				self.matrix[i][j] = int(input())
-	def show(self):
-		for i in range(self.n):
-			for j in range(self.m):
-				print(str(self.matrix[i][j]).ljust(1), end=' ')
-			print()
+		    for j in range(m):
+		        new_matrix[i][j] = int(input(f"Элемент [{i}][{j}]: "))
+		self.arr_matrix.append(new_matrix) 
+
+	def show(self, matrix):
+		for row in matrix:
+			print(' '.join(map(str, row)))
+		print()
 
 	def transponse(self):
-		for i in range(self.n):
-			for j in range(i + 1, self.n):
-				self.matrix[i][j], self.matrix[j][i] = self.matrix[j][i], self.matrix[i][j]
-		print('Транспонированная матрица')
-		self.show()
+		for matr in self.arr_matrix:
+			for i in range(len(matr)):
+				for j in range(i + 1, len(matr)):
+					matr[i][j], matr[j][i] = matr[j][i], matr[i][j]
+		print('Транспонированная матрица:')
+		self.show(matr)
 
 	def main_diag(self):
 		self.main = []
-		c = 0
-		for i in range(self.n):
-			self.main.append(self.matrix[i][c])
-			c += 1
-		print('Главная диагональ')
+		for matr in self.arr_matrix:
+			c = 0
+			h = []
+			for i in range(len(matr)):
+				h.append(matr[i][c])
+				c += 1
+			self.main.append(h)
 		print(self.main)
 
+	def my_matrix(self):
+		for matrix in self.arr_matrix:
+			self.show(matrix)
 
+	def mul_matrix(self):
+		print('Введите новую матрицу')
+		
+
+
+
+a = Linear_Bsk_Operations()
 n = int(input('n = '))
 m = int(input('m = '))
-a = Linear_Bsk_Operations(n, m)
-a.transponse()
+a.add_matrix(n, m)
+n_2 = int(input('n_2 = '))
+m_2 = int(input('m_2 = '))
+a.add_matrix(n_2, m_2)
+a.my_matrix()
+# a.transponse()
 a.main_diag()
+# a.mul_matrix()
